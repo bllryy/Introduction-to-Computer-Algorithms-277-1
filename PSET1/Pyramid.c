637fc7ee2d684915f2e20bb52f408e04934db8f9
lily <bllryy@proton.me>
2025-09-08 06:25:26 -0700

    changes

--- PSET1/Pyramid.c
+++ PSET1/Pyramid.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    char one[] = "    *";
-    char three[] = "   ***";
-    char five[] = "  *****";
-    char seven[] = " *******";
-    char nine[] = "*********";
+	int height = 5;
 
-    printf("%s\n", one);
-    printf("%s\n", three);
-    printf("%s\n", five);
-    printf("%s\n", seven);
-    printf("%s\n", nine);
+    	for (int row = 1; row <= height; row++) {
+        	for (int space = 1; space <= height - row; space++) {
+            		printf(" ");
+        }
+        
+        	for (int star = 1; star <= 2 * row - 1; star++) {
+            		printf("*");
+        }
+        
+        printf("\n");
+    }
+    
     return 0;
 }
+
+	
+	

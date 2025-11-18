aade6faa45725b8206ac05b631f6eff070205b97
lily <bllryy@proton.me>
2025-11-18 11:16:54 -0500

    commit

---
+++ PSET5/jump.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int minJumps(int arr[], int n)
+{
+    if (arr[0] == 0) return -1;
+
+    int jumps = 0;
+    int currReacy = 0;
+    int maxReach = 0;
+
+    
+    for (int i = 0; i < n - 1; i++)
+    {
+        maxReach = (maxReach > i + arr[i]) ? maxReach : (i + arr[i]);
+
+        if (maxReach >= n - 1)
+        {
+            return jumps + 1;
+        }
+
+        // Current Reachable index increment the Jump as we reached the reach able index 
+        if (i == currReacy)
+        {
+            return  -1;
+        }
+
+        else {
+            jumps++;
+            currReacy = maxReach;
+        }
+    }
+    return -1;
+
+}
+
+
+
+int main()
+{
+    int arr[] = {1, 3, 2, 2, 1, 4, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("%d\n", minJumps(arr, n));
+    return 0;
+}
--- PSET5/mincost.c
+++ PSET5/mincost.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <limits.h>  //INT_MAX 
+// https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.geeksforgeeks.org/cpp/int_max-int_min-cc-applications/&ved=2ahUKEwjpk_bfr_mQAxXZFlkFHXDUO-8QFnoECBgQAQ&usg=AOvVaw1oXBb3wFFIg_0hlEWuSh1M
 
-int cost()
-{
+int cost(int arr[], int n) {
+    int min_element = INT_MAX;  
 
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min_element)
+        {
+            min_element = arr[i];
+        }
+    }
 
+    return (n - 1) * min_element;
 }
 
-
-int main() 
-{
-    arr[]= {4 ,3 ,2 };
+int main() {
+    int arr[] = {4, 3, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("minimum cost: %d\n", cost(arr, n));  // Output the total cost
+
+    return 0;
 }

859532e033f3edb5ef040ef35c1dbe353599f463
lily <bllryy@proton.me>
2026-02-28 02:44:45 -0500

    pset 7

--- PSET7/main.c
+++ PSET7/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <threads.h>
 
 typedef struct Block {
     int index;
@@ -43,51 +42,60 @@ int compute_hash(int index, int prev_hash, const char *data)
 
 void init_blockchain(Blockchain *bc)
 {
-  Block *genesis = (Block *)malloc(sizeof(Block));
-  // always at 0, always GENESIS, prev hash 0
-  genesis->index = 0;
-  strcpy(genesis->data, "GENESIS");
-  genesis->prev_hash = 0;
-  genesis->next = NULL;
+	bc->head = (Block *)malloc(sizeof(Block));
+	bc->head->index = 0;
+	strcpy(bc->head->data, "GENESIS");
+	bc->head->prev_hash = 0;
+	bc->head->hash = compute_hash(bc->head->index, bc->head->prev_hash, bc->head->data);
+	bc->head->next = NULL;
+
+	bc->tail = bc->head;
+	bc->length = 1;
 }
 
 int add_block(Blockchain *bc, const char *data)
 {
-  // malloc new block
-  Block* new_block = (Block*)malloc(sizeof(Block));
-  new_block->data = data;
+	Block* new_block = (Block*)malloc(sizeof(Block));
+	new_block->index = bc->tail->index + 1;
+	new_block->prev_hash = bc->tail->hash;
+	strcpy(new_block->data, data);
+	new_block->hash = compute_hash(new_block->index, new_block->prev_hash,new_block->data);
+	new_block->next = NULL;
 
-  // set last hash
-  if (bc->tail != NULL)
-  {
-    strcpy(new_block->prev_hash, bc->tail->hash);
-    bc->head = new_block;
-  } else {
-    strcpy(new_block->prev_hash, "0");
-    bc->head = new_block;
-  }
-
-  compute_hash(new_block);
-
-  return 0;
-
+	bc->tail->next = new_block;
+	bc->tail = new_block;
+	bc->length++;
+	return 0;
 }
 
 void print_chain(const Blockchain *bc) {
-  struct Block* current = chain;
-  int count = 0;
-
+Block *current = bc->head;
   while (current != NULL) {
-      printf(" Block: %d\n", count++);
+      printf(" Block: %d\n", current->index);
       printf("  Index: %d\n", current->index);
-      printf("  Hash: %s\n", current->hash);
+      printf("  Hash: %d\n", current->hash);
       printf("  Data: %s\n", current->data);
       current = current->next; // next block
   }
 }
 
+// CHECK work
 int is_valid_chain(const Blockchain *bc)
 {
+    if (bc->head->hash != compute_hash(bc->head->index, bc->head->prev_hash, bc->head->data)) return 0; 
+
+    Block *current = bc->head;
+    while (current != NULL) {
+        if (current->index != (current->prev_hash == 0 ? 0 : current->index - 1)) {
+            return 0;
+        }
+
+        int expected_hash = compute_hash(current->index, current->prev_hash, current->data);
+        if (current->hash != expected_hash) return 0; 
+
+        current = current->next; 
+    }
+    return 1; 
 
 }
 
@@ -103,54 +111,107 @@ void free_blockchain(Blockchain *bc) {
   }
 }
 
-Peer *add_peer(Peer *net, int id) {
+Peer *add_peer(Peer *net, int id) 
+{
+	Peer *new_peer = (Peer *)malloc(sizeof(Peer));
 
+	new_peer->id = id;
+	init_blockchain(&new_peer->chain);
+	new_peer->next = net;
+	return new_peer;
 }
 
-//TODO
-Peer* find_peer(Peer *net, int id) {
-  i = 0;
-  while (i < peer_count) {
-    if (peers[i].id == search_id) {
-        return &peers[i];
+Peer* find_peer(Peer *net, int id) 
+{
+  while (net != NULL)
+{
+    if (net->id == id)
+{
+        return net;
     }
-    i++;
+    net = net->next;
   }
   return NULL;
 }
 
 //TODO
-void print_network(const Peer *net) {
-  for(i = 0; i < peer_count; i++) {
-    printf("Peer ID: %d, Length: %d, Validity: %s\n",
-           peers[i].id, peers[i].length, peers[i].is_valid ? "Valid" : "Invalid");
-    // if exist print
-    if (peers[i].blockchain != NULL) {
-        printf("Details for peer id %d:\n", peers[i].id);
-        print_chain(peers[i].blockchain);
-    }
-  }
+void print_network(const Peer *net) 
+{
+const Peer *current = net;
+    while (current != NULL) 
+{
+        printf("Peer ID: %d, Length: %d, Validity: %s\n",
+               current->id, current->chain.length, is_valid_chain(&current->chain) ? "Valid" : "Invalid");
+        print_chain(&current->chain); // Print chain details
+        current = current->next; // Move to the next peer
+}
 }
 
-void broadcast(Peer *net, const char *data) {
-
+void broadcast(Peer *net, const char *data) 
+{
+	while(net != NULL)
+	{
+		if(is_valid_chain(&net->chain))
+		{
+			add_block(&net->chain, data);
+		}
+		net = net->next;
+	}
 }
 
-int tamper_peer(Peer *net, int peer_id, int block_index, const char *new_data) {
-
+int tamper_peer(Peer *net, int peer_id, int block_index, const char *new_data) 
+{
+	Peer *peer = find_peer(net, peer_id);
+		Block *current = peer->chain.head;
+    		for (int i = 0; i < block_index; i++) 
+		{
+        		if (current == NULL) return -1; 
+        		current = current->next; 
+    		}
+    	strcpy(current->data, new_data); // 
+    	return 0;
 }
 
-Peer* consensus_winner(Peer *net) {
+Peer* consensus_winner(Peer *net) 
+{
+Peer *winner = NULL;
+int max_length = 0;
 
+	while (net != NULL) 
+	{
+			if (is_valid_chain(&net->chain) && net->chain.length > max_length) 
+		{
+			max_length = net->chain.length;
+			winner = net; 
+        	}
+	net = net->next; 
+    	}
+	return winner; 
 }
 
 
-void apply_consensus(Peer *net) {
-
+void apply_consensus(Peer *net) 
+{
+	Peer *winner = consensus_winner(net);
+	
+	while(net != NULL)
+	{
+		free_blockchain(&net->chain);
+		net->chain = winner->chain;
+		net=net->next;
+	}
 }
 
-void free_network(Peer *net) {
-
+void free_network(Peer *net) 
+{
+	Peer *current = net;
+	while(current != NULL)
+	{
+		Peer *next_peer = current->next;
+		free_blockchain(&current->chain);
+		free(current);
+		current = next_peer;
+	}
 }
 
 
